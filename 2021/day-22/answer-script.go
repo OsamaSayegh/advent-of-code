1b1bafce367aa3a38dd2825e33fd0e1f2244d49a
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-01-02 10:55:55 +0300

    day 22 prog

--- 2021/day-22/answer-script.go
+++ 2021/day-22/answer-script.go
@@ -8,13 +8,17 @@ import (
 )
 
 type Step struct {
-	x1 int
-	x2 int
-	y1 int
-	y2 int
-	z1 int
-	z2 int
+    Cuboid
 	on bool
+}
+
+type Cuboid struct {
+  x int
+  y int
+  z int
+  w int
+  h int
+  d int
 }
 
 func parseRange(coords string) (int, int) {
@@ -25,6 +29,22 @@ func parseRange(coords string) (int, int) {
 		panic(err)
 	}
 	return v1, v2
+}
+
+func subtract(a, b Cuboid) []Cuboid {
+  res := []Cuboid
+  topHeight := b.y - a.y
+  if topHeight > 0 {
+    top := Cuboid{
+      x: a.x,
+      y: a.y,
+      z: a.z,
+      w: a.w,
+      h: topHeight,
+      d: a.d,
+    }
+    res = append(res, top)
+  }
 }
 
 func run() int {
@@ -48,33 +68,20 @@ func run() int {
 		x := strings.Replace(coords[0], "x=", "", 1)
 		y := strings.Replace(coords[1], "y=", "", 1)
 		z := strings.Replace(coords[2], "z=", "", 1)
-		step.x1, step.x2 = parseRange(x)
-		step.y1, step.y2 = parseRange(y)
-		step.z1, step.z2 = parseRange(z)
+        x1, x2 := parseRange(x)
+		y1, y2 := parseRange(y)
+		z1, z2 := parseRange(z)
+        step.x = x1
+        step.y = y1
+        step.z = z1
+        step.w = x2 - x1
+        step.h = y2 - y1
+        step.d = z2 - z1
 		steps = append(steps, step)
 	}
-	lit := make(map[[3]int]bool)
-	fmt.Println("steps", len(steps))
-	for i, step := range steps {
-		fmt.Println(i)
-		for z := step.z1; z <= step.z2; z++ {
-			// if z < -50 || z > 50 { continue }
-			for y := step.y1; y <= step.y2; y++ {
-				// if y < -50 || y > 50 { continue }
-				for x := step.x1; x <= step.x2; x++ {
-					// if x < -50 || x > 50 { continue }
-					loc := [3]int{x, y, z}
-					if step.on {
-						lit[loc] = true
-					} else {
-						delete(lit, loc)
-					}
-				}
-			}
-		}
-	}
-	fmt.Println(len(lit))
-	fmt.Println(steps)
+
+    fmt.Println(steps)
+    // cuboids := []Cuboid{}
 	return 0
 }
 
