1a11ebc598ec96631173c625a7db71ca9f841394
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-01-18 14:08:57 +0300

    day 19 changes

--- 2021/day-19/answer-script.go
+++ 2021/day-19/answer-script.go
@@ -8,14 +8,46 @@ import (
     "math"
 )
 
+var Rotations = [...][3]int {
+  // multiples of 90-degress for the x, y, z axes
+  {0, 0, 0},
+  {0, 1, 0},
+  {0, 2, 0},
+  {0, 3, 0},
+
+  {0, 0, 1},
+  {0, 1, 1},
+  {0, 2, 1},
+  {0, 3, 1},
+
+  {0, 0, 2},
+  {0, 1, 2},
+  {0, 2, 2},
+  {0, 3, 2},
+
+  {0, 0, 3},
+  {0, 1, 3},
+  {0, 2, 3},
+  {0, 3, 3},
+
+  {1, 0, 0},
+  {1, 1, 0},
+  {1, 2, 0},
+  {1, 3, 0},
+
+  {3, 0, 0},
+  {3, 1, 0},
+  {3, 2, 0},
+  {3, 3, 0},
+}
+
 type Beacon struct {
-  x int
-  y int
-  z int
+  position [3]int
 }
 
 type Scanner struct {
 	beacons []*Beacon
+    id int
 }
 
 func abs(v int) int {
@@ -33,7 +65,35 @@ func parse(xyz string) (int, int, int) {
 	return x, y, z
 }
 
+func rz(pos [3]int, steps int) [3]int {
+  for steps != 0 {
+    pos[0], pos[1], pos[2] = -pos[1], pos[0], pos[2]
+    steps -= 1
+  }
+  return pos
+}
+
+func rx(pos [3]int, steps int) [3]int {
+  for steps != 0 {
+    pos[0], pos[1], pos[2] = pos[0], pos[2], -pos[1]
+    steps -= 1
+  }
+  return pos
+}
+
+func ry(pos [3]int, steps int) [3]int {
+  for steps != 0 {
+    pos[0], pos[1], pos[2] = -pos[2], pos[1], pos[0]
+    steps -= 1
+  }
+  return pos
+}
+
 // 0 -> 1 -> 4 -> 2 -> 3
+// scanner 1 @ 68,-1246,-43
+// scanner 4 @ -20,-1133,1061
+// scanner 2 @ 1105,-1205,1229
+// scanner 3 @ -92,-2380,-20
 
 func run() int {
 	data, err := os.ReadFile("input.txt")
@@ -41,23 +101,56 @@ func run() int {
 		panic(err)
 	}
 	scanners := []*Scanner{}
-	for _, beacons := range strings.Split(strings.TrimSpace(string(data)), "\n\n") {
+	for i, beacons := range strings.Split(strings.TrimSpace(string(data)), "\n\n") {
 		scanner := &Scanner{}
+        scanner.id = i
 		for i, position := range strings.Split(beacons, "\n") {
 			if i == 0 {
 				continue
 			}
-			x, y, z := parse(position)
             beacon := &Beacon{}
-            beacon.x = x
-            beacon.y = y
-            beacon.z = z
+			x, y, z := parse(position)
+            beacon.position = [3]int{x, y, z}
 			scanner.beacons = append(scanner.beacons, beacon)
 		}
 		scanners = append(scanners, scanner)
 	}
-    fmt.Println(scanners[0].beacons[0])
-    fmt.Println(len(scanners))
+    // sorted := []*Scanner{scanners[0]}
+    // for len(sorted) != len(scanners) {
+    //   last := sorted[len(sorted)-1]
+    //   for _, scanner := range scanners {
+    //     skip := false
+    //     for _, done := range sorted {
+    //       if done.id == scanner.id {
+    //         skip = true
+    //         break
+    //       }
+    //     }
+    //     if skip {
+    //       continue
+    //     }
+    //     for _, rotation := range Rotations {
+    //       rotated := &Scanner{}
+    //       for _, b := range scanner.beacons {
+    //         pos := b.position
+    //         x, y, z := rotation[0], rotation[1], rotation[2]
+    //         if z != 0 {
+    //           pos = rz(pos, z)
+    //         }
+    //         if x != 0 {
+    //           pos = rx(pos, x)
+    //         }
+    //         if y != 0 {
+    //           pos = ry(pos, y)
+    //         }
+    //         rotated.beacons = append(rotated.beacons, &Beacon{pos})
+    //       }
+    //     }
+    //   }
+    // }
+    arr := [3]int{1,2,3}
+    fmt.Println(ry(rx(arr, 2), 1))
+    fmt.Println(ry(rz(arr, 2), 3))
 	return 0
 }
 
